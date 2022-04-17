3e71155103dc37f3eb9b2f7f4ee26c1508bc3d66
naamagi <n.gilat@gmail.com>
2022-04-18 01:49:09 +0300

    update

--- 13.2-sort_strings.js
+++ 13.2-sort_strings.js
@@ -1,11 +1,31 @@
-
-
 const foods = ["falafel", "sabich", "hummus", "pizza with extra pineapple"];
-
+//a1
 console.log(foods.slice().sort());
-
+//a2
 console.log(foods.slice().sort((a, b) => b.localeCompare(a)));
 
+const foodsWithUpperCase = [
+  "falafel",
+  "Sabich",
+  "hummus",
+  "pizza with extra pineapple",
+];
+//b1
+console.log(
+  foodsWithUpperCase
+    .slice()
+    .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+);
+//b2
+console.log(
+  foodsWithUpperCase
+    .slice()
+    .sort((a, b) => b.toLowerCase().localeCompare(a.toLowerCase()))
+);
 
+const words = ["apple", "supercalifragilisticexpialidocious", 
+"hi", "zoo"];
 
-console.log(foods.slice().sort((a, b) => b.toLowerCase().localeCompare(a.toLowerCase())));
+//c1
+
+console.log(words.sort((a,b) =>b.length - a.length));
